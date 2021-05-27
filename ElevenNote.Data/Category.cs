8f30ad8069fce617d560fdc5b0426fa00c000b73
Raymond Sipes <raymondsipes21@gmail.com>
2021-05-27 14:57:55 -0400

    Added a whole buch of bullshit in swaggerconfig, couldnt tell you a damn thing about it either

---
+++ ElevenNote.Data/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    //public class Category
+    //{
+    //    [ForeignKey(Title)]
+    //    public int CategoryId { get; set; }
+    //    public string Name { get; set; }
+    //} DO THIS AFTER FINISHING ORIGINAL PROGRAM
+}
